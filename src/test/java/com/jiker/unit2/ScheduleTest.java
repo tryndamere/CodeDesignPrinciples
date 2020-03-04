1f54b669fd3a8942441a4927b56858f38fdb40b7
rocky <cxilymh@gmail.com>
2020-03-04 23:26:14 +0800

    修改测试类方法命名

--- src/test/java/com/jiker/unit2/ScheduleTest.java
+++ src/test/java/com/jiker/unit2/ScheduleTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class ScheduleTest {
 
     @Test
-    public void should_schedule_swan_action() {
+    public void should_schedule_animal_action() {
         Schedule schedule = new Schedule(Lists.newArrayList(new WildGoose(), new Swallow(), new Penguin()));
         schedule.print();
     }
